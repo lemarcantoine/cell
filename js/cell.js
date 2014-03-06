564e0b3ae53f3211e99be5c020dc98caee58538f
N3k <ebk0000@gmail.com>
2014-03-06 14:59:08 +0100

    Parameters + better IA
    J'ai ajouté la première IA qui suis les autres cellules et dessine une
    ligne.
    L'objet param permet de modifier des paramêtres dans la console.

--- js/cell.js
+++ js/cell.js
@@ -2,6 +2,7 @@ function Cell( x, y ) {
   this.world = null;
   this.x = ( typeof x != "undefined" ) ? x : 0;
   this.y = ( typeof y != "undefined" ) ? y : 0;
+  this.target = this;
   this.oldX = this.x;
   this.oldY = this.y;
   this.hue = Math.random() * 360;
@@ -15,13 +16,28 @@ Cell.prototype = {
   },
 
   render : function() {
+
     var ctx = this.world.ctx;
-    
+    if(this.target!=this){
+    	ctx.beginPath();
+	    ctx.moveTo(this.x + this.world.centerX, this.y + this.world.centerY);
+	    ctx.lineTo(this.target.x + this.world.centerX, this.target.y + this.world.centerY);
+		ctx.strokeStyle="hsl(" + this.hue + ", 90%, 75% )";
+	    ctx.lineWidth = param.lineWidth;
+		ctx.stroke();
+	    ctx.font="8px Arial";
+	    var distance = G.distance( this.x, this.y, this.target.x, this.target.y ) | 0;
+	    var offsetTextX = -10,
+	    	offsetTextY = -13;
+	    ctx.fillText(distance,this.x + this.world.centerX + offsetTextX, this.y + this.world.centerY + offsetTextY);
+	}
+
     ctx.fillStyle = "hsl(" + this.hue + ", 90%, 50% )";
-    
     ctx.beginPath();
     ctx.arc( this.x + this.world.centerX, this.y + this.world.centerY, 10, 0, 2 * Math.PI );
     ctx.fill();
+    
+
   },
   
   radar : function( min ) {
@@ -33,11 +49,53 @@ Cell.prototype = {
     this.oldY = this.y;
     this.oldHue = this.hue;
   },
-  
+
   tick : function() {
-    var nearestCell = this.radar();
+  	var direction = 0, distance=0;
+  	var seedX = ((this.seed*777)%50)/50
+    var seedY = ((this.seed*993)%50)/50
+    //Random tremblote
+    this.x += (Math.random()-0.5) * (seedX)*2;// 0
+    this.y += (Math.random()-0.5) * (seedY)*2;// 1.5
+    var oldTargetSeeds=[];
+	    	
+  	while(direction==0){
+
+	    var nearestCell = this.radar(distance+.1);
+	    
+	    if( nearestCell === null) {
+	    	return;
+	    }
+	    var i = oldTargetSeeds.length;
+	    while(i--){
+	    	if(oldTargetSeeds[i] == nearestCell.seed)
+	    		return;
+	    }
+
+		
+	    oldTargetSeeds[oldTargetSeeds.length]=nearestCell.seed;
+	    
+	    var distance = G.distance( this.x, this.y, nearestCell.oldX, nearestCell.oldY );
+		
+		if( distance > param.minDistance + param.deadZone + this.seed * 10 ) direction = 1;
+		else if( distance < param.minDistance + this.seed * 10 ) direction = -1;
+		else continue;
+
+
+		if(distance!=0){
+			this.x += ( nearestCell.oldX - this.x ) / distance * direction ;
+			this.y += ( nearestCell.oldY - this.y ) / distance * direction ;
+		}
+		
+		this.target=nearestCell;
+
+  	}
+    	this.hue += ( ( nearestCell.oldHue - this.hue)/nearestCell.oldHue + 360 ) % 360;
+
     
-    if( nearestCell === null ) return;
+    
+    
+
     /*
     var angle = G.angleTo( this.x, this.y, nearestCell.x, nearestCell.y ) -Math.PI/2,
         distance = G.distance( this.x, this.y, nearestCell.x, nearestCell.y ),
@@ -49,15 +107,9 @@ Cell.prototype = {
     this.y += -Math.cos( angle ) * direction;
     */
     
-    var distance = G.distance( this.x, this.y, nearestCell.oldX, nearestCell.oldY ),
-        direction = -1;
-    if( distance > 20 + this.seed * 10 ) direction = 1;
     
-    this.hue = ( nearestCell.oldHue - this.hue + 360 ) % 360;
-    
-    this.x += (nearestCell.oldX - this.x) / distance * direction + Math.random() * this.seed * 2 - 1;
-    this.y += (nearestCell.oldY - this.y) / distance * direction + Math.random() * this.seed * 2 - 1;
-    
-  }
+
+
+   }
 
 }
